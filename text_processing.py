764d0e1e8d1ae6ba05dc4ec3b14ceb8cfbc06513
Julia117 <julialysenko08@gmail.com>
2020-07-10 12:07:58 +0200

    move text processing functions to another file

--- text_processing.py
+++ text_processing.py
@@ -33,3 +33,71 @@ def num_replace(word):
     newtoken = 'x' * len(word)
     nw = newtoken + '_NUM'
     return nw
+
+def process(pipeline, text='Строка', keep_pos=True, keep_punct=False):
+    entities = {'PROPN'}
+    named = False
+    memory = []
+    mem_case = None
+    mem_number = None
+    tagged_propn = []
+
+    # обрабатываем текст, получаем результат в формате conllu:
+    processed = pipeline.process(text)
+
+    # пропускаем строки со служебной информацией:
+    content = [l for l in processed.split('\n') if not l.startswith('#')]
+
+    # извлекаем из обработанного текста леммы, тэги и морфологические характеристики
+    tagged = [w.split('\t') for w in content if w]
+
+    for t in tagged:
+        if len(t) != 10:
+            continue
+        (word_id, token, lemma, pos, xpos, feats, head, deprel, deps, misc) = t
+        token = clean_token(token, misc)
+        lemma = clean_lemma(lemma, pos)
+        if not lemma or not token:
+            continue
+        if pos in entities:
+            if '|' not in feats:
+                tagged_propn.append('%s_%s' % (lemma, pos))
+                continue
+            morph = {el.split('=')[0]: el.split('=')[1] for el in feats.split('|')}
+            if 'Case' not in morph or 'Number' not in morph:
+                tagged_propn.append('%s_%s' % (lemma, pos))
+                continue
+            if not named:
+                named = True
+                mem_case = morph['Case']
+                mem_number = morph['Number']
+            if morph['Case'] == mem_case and morph['Number'] == mem_number:
+                memory.append(lemma)
+                if 'SpacesAfter=\\n' in misc or 'SpacesAfter=\s\\n' in misc:
+                    named = False
+                    past_lemma = '::'.join(memory)
+                    memory = []
+                    tagged_propn.append(past_lemma + '_PROPN ')
+            else:
+                named = False
+                past_lemma = '::'.join(memory)
+                memory = []
+                tagged_propn.append(past_lemma + '_PROPN ')
+                tagged_propn.append('%s_%s' % (lemma, pos))
+        else:
+            if not named:
+                if pos == 'NUM' and token.isdigit():  # Заменяем числа на xxxxx той же длины
+                    lemma = num_replace(token)
+                tagged_propn.append('%s_%s' % (lemma, pos))
+            else:
+                named = False
+                past_lemma = '::'.join(memory)
+                memory = []
+                tagged_propn.append(past_lemma + '_PROPN ')
+                tagged_propn.append('%s_%s' % (lemma, pos))
+
+    if not keep_punct:
+        tagged_propn = [word for word in tagged_propn if word.split('_')[1] != 'PUNCT']
+    if not keep_pos:
+        tagged_propn = [word.split('_')[0] for word in tagged_propn]
+    return tagged_propn
