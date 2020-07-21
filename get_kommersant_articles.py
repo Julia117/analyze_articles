343ac2f3f5a52a2adb6aad6b51481d2270506af2
Julia117 <julialysenko08@gmail.com>
2020-07-21 13:38:58 +0200

    fix retrieve href from html

--- get_kommersant_articles.py
+++ get_kommersant_articles.py
@@ -20,13 +20,11 @@ def get_articles_urls_for_date(date_raw):
     html_content = requests.get(url).text
     html_soup = BeautifulSoup(html_content, "lxml")
     urls = []
-    # result = html_soup.find_all('article',attrs={"class": "uho"})
-    for c in html_soup.find_all('article',attrs={"class": "uho"}):
-        content = c.contents[1]
-        if ('href' in content.attrs):
-            href = content.attrs['href']
-            url = ("https://www.kommersant.ru" + href)
-            urls.append(url)
+    # result = html_soup.find_all('article',attrs={"class": "archive_result__item_text"})
+    for c in html_soup.find_all(attrs={"class": "archive_result__item_text"}):
+        href = c.contents[1].attrs['href']
+        url = ("https://www.kommersant.ru" + href)
+        urls.append(url)
     return urls
 
 get_articles_urls_for_date(date_raw)
